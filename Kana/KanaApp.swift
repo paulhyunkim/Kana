6053a5bceddee6989d64925b2280f37dcdaa9499
Paul Kim <paulhyunkim@gmail.com>
2024-10-10 16:53:11 -0700

    Initial Commit

---
+++ Kana/KanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaApp.swift
+//  Kana
+//
+//  Created by Paul Kim on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct KanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
